de90cb380fa1df5a90685e41ea978b4fc3c014ca
Antoni Remeseiro Alfonso <antoniremeseiro@gmail.com>
2026-06-03 15:47:57 +0900

    refactor: refactored header into it's own component
    Refactored the header (<H1> tag) into it's own component with props to pass different course strings to use as title.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,8 @@
+const Header = (props) => {
+  console.log(props);
+  return <h1>{props.course}</h1>;
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -9,7 +14,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
+      <Header course={course} />
       <p>
         {part1} {exercises1}
       </p>
