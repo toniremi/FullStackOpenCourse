3cf50a904764fc28d475a9991201d3d540765c3e
Antoni Remeseiro Alfonso <antoniremeseiro@gmail.com>
2026-06-09 15:07:06 +0900

    refactor: changed course and part variables into a single javascript object
    Following exercise 1.5 I changed the course and parts variable into a single javascript object. Then modified the props passed to each component to use the appropriate javascript object parameter.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -31,20 +31,30 @@ const Total = (props) => {
 };
 
 const App = () => {
-  // course titlle
-  const course = "Half Stack application development";
-  // course parts into an array
-  const parts = [
-    { name: "Fundamentals of React", exercises: 10 },
-    { name: "Using props to pass data", exercises: 7 },
-    { name: "State of a component", exercises: 14 },
-  ];
+  // course information object
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
