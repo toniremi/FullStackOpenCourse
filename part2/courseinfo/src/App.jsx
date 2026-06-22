ded8899abba0e44a14e19b12a402baa05f76a822
Antoni Remeseiro Alfonso <antoniremeseiro@gmail.com>
2026-06-22 14:51:17 +0900

    refactor: exercise 2.1 render parts using array.map
    Changed the way we render the course parts to use array.map so we can render any number of parts across different course dynamically.

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -13,9 +13,9 @@ const Course = (props) => {
 
 const Content = (props) => (
   <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
+    {props.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
   </div>
 );
 
