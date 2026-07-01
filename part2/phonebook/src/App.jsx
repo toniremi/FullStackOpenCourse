3181d353b957a215c5988817c094ad433fa002de
Antoni Remeseiro Alfonso <antoniremeseiro@gmail.com>
2026-07-01 15:03:08 +0900

    feat: exercise 2.6 add person to the phone book functionality
    Added two event handlers to set the name on the proper state and then an event handler to capture the form submit so we can add the new person into our persons array state.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,12 +4,30 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  // submit event handler for the form, which creates a new person object and adds it to the persons array, then clears the input field
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+    const personObject = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  // create the event handler for the input field and set the value of newName to the value of the input field
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
